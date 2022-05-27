3bfe396ac5860839f847e5bca0de7a167fbe117b
Kyle Moad <kyle.moad@gmail.com>
2022-05-26 22:21:31 -0400

    make cli program

--- letter-boxed.py
+++ letter-boxed.py
@@ -1,20 +1,15 @@
 from collections import defaultdict
 
-words = set()
-with open('words.txt') as f:
-    for l in f:
-        words.add(l.strip())
-
 class PrefixNode(object):
     def __init__(self, parent=None):
         self.children = {}
         self.parent = parent
         self.word = None
-        # self.letter = None
+        self.letter = None
 
     def add(self, word, n=0):
-        # if n>0:
-        #     self.letter = word[n-1]
+        if n>0:
+            self.letter = word[n-1]
         if n == len(word):
             self.word = word
         else:
@@ -24,28 +19,28 @@ class PrefixNode(object):
                 self.children[next_letter] = PrefixNode(parent=self)
             self.children[next_letter].add(word, n=next_n)
     
-    # def __getitem__(self, letter):
-    #     return self.children[letter]
+    def __getitem__(self, letter):
+        return self.children[letter]
     
-    # def __iter__(self):
-    #     for child in self.children.values():
-    #         yield child
+    def __iter__(self):
+        for child in self.children.values():
+            yield child
 
-    # def check(self, prefix, leaf=False):
-    #     if len(prefix) == 1:
-    #         if leaf:
-    #             return self.word is not None and self.word[-1] == prefix
-    #         else:
-    #             return prefix in self.children
-    #     else:
-    #         if prefix[0] in self.children:
-    #             return self.children[prefix[0]].check(prefix[1:], leaf=leaf)
-    #         else:
-    #             return False
+    def check(self, prefix, leaf=False):
+        if len(prefix) == 1:
+            if leaf:
+                return self.word is not None and self.word[-1] == prefix
+            else:
+                return prefix in self.children
+        else:
+            if prefix[0] in self.children:
+                return self.children[prefix[0]].check(prefix[1:], leaf=leaf)
+            else:
+                return False
 
-    # def count(self):
-    #     cnt = 1 if self.word else 0
-    #     return cnt + sum([_.count() for _ in self.children.values()])
+    def count(self):
+        cnt = 1 if self.word else 0
+        return cnt + sum([_.count() for _ in self.children.values()])
 
     def lb_iter(self, letter_box, last_group=None):
         words = []
@@ -56,11 +51,10 @@ class PrefixNode(object):
             if can_continue:
                 words += child.lb_iter(letter_box, last_group=this_group)
         return words
-        
 
 class LetterBox(object):
     def __init__(self, sides):
-        self.sides = sides
+        self.sides = [list(_) for _ in sides]
         self.all_letters = set()
         for side in sides:
             self.all_letters |= set(''.join(side))
@@ -79,55 +73,19 @@ class LetterBox(object):
         query_letters = set(''.join(words))
         return query_letters == self.all_letters
 
-# 2022-05-26
-lb = LetterBox([
-    ['N','E','P'],
-    ['U','L','A'],
-    ['B','D','T'],
-    ['I','C','M'],
-])
-# 2022-05-25
-# lb = LetterBox([
-#     ['D','T','E'],
-#     ['C','R','V'],
-#     ['A','O','Y'],
-#     ['I','H','L'],
-# ])
-# https://medium.com/@pnmclaugh/solving-letter-boxed-in-python-5e6cc4b216f9
-# lb = LetterBox([
-#     ['S','M','A'],
-#     ['I','L','P'],
-#     ['T','N','E'],
-#     ['O','C','Z'],
-# ])
-# print(lb.all_letters)
-
-tree = PrefixNode()
-for word in words:
-    tree.add(word)
-
-valid_words = [_ for _ in tree.lb_iter(lb) if len(_)>2]
-# print(len(valid_words))
-valid_words.sort(key=lambda _: len(_), reverse=True)
-# print(valid_words)
-words_by_start = defaultdict(list)
-for word in valid_words:
-    words_by_start[word[0]].append(word)
-
 # This is a combinatorial problem. Work should be done to reduce the difficulty.
 # But it will be impossible to avoid the O(n^k) complexity completely. Thankfully,
 # n is at least somewhat bounded
-def get_solution(valid_words, words_by_start, letter_box, prev_chain=[], max_length=5):
+def get_solution(words, words_by_start, letter_box, prev_chain=[], max_length=5):
     solutions = []
     if prev_chain:
         cur_words = words_by_start[prev_chain[-1][-1]]
     else:
-        cur_words = valid_words
+        cur_words = words
     # Check for solution this iteration
     for word in cur_words:
         cur_chain = prev_chain + [word]
         if letter_box.check_coverage(cur_chain):
-            # print(cur_chain)
             solutions.append(cur_chain)
     if solutions:
         return solutions
@@ -137,13 +95,45 @@ def get_solution(valid_words, words_by_start, letter_box, prev_chain=[], max_len
     # Check next iteration
     for word in cur_words:
         cur_chain = prev_chain + [word]
-        next_solutions = get_solution(valid_words, words_by_start, letter_box, prev_chain=cur_chain, max_length=max_length)
+        next_solutions = get_solution(words, words_by_start, letter_box, prev_chain=cur_chain, max_length=max_length)
         if next_solutions:
             solutions += next_solutions
     return solutions
-    # return []
 
-solutions = get_solution(valid_words, words_by_start, lb, max_length=2)
-# print(len(solutions))
-for solution in solutions:
-    print('\t'.join(solution))
+if __name__ == '__main__':
+    from argparse import ArgumentParser
+    from pathlib import Path
+
+    parser = ArgumentParser()
+    parser.add_argument('letters',
+        help='Format: ABC-DEF-GHI-JKL',
+    )
+    parser.add_argument('-l','--length',
+        type=int,
+        default=2,
+        help='Max solution length',
+    )
+    parser.add_argument('-w','--words',
+        type=Path,
+        default=Path('words_easy.txt'),
+        help='File containing words',
+    )
+    args = parser.parse_args()
+
+    all_words = args.words.read_text().split('\n')
+    valid_words = [_ for _ in all_words if len(_)>2]
+    prefix_tree = PrefixNode()
+    for word in set(valid_words):
+        prefix_tree.add(word)
+
+    letter_box = LetterBox(args.letters.upper().split('-'))
+
+    solution_words = [_ for _ in prefix_tree.lb_iter(letter_box) if len(_)>2]
+    solution_words.sort(key=lambda _: len(_), reverse=True)
+    by_start = defaultdict(list)
+    for word in solution_words:
+        by_start[word[0]].append(word)
+    
+    solutions = get_solution(solution_words, by_start, letter_box, max_length=2)
+    for solution in solutions:
+        print(' '.join(solution))
